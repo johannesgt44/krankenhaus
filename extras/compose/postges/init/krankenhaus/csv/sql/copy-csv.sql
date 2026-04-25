5c2b4901ae5b03bea7d52b29270326e7bf4a1b9d
Johannes <johannesgt44@gmail.com>
2026-04-25 16:02:02 +0200

    added sql dateien

---
+++ extras/compose/postges/init/krankenhaus/csv/sql/copy-csv.sql
@@ -0,0 +1,24 @@
+-- Copyright (C) 2022 - present Juergen Zimmermann, Hochschule Karlsruhe
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-- Aufruf:   psql --dbname=krankenhaus --username=postgres --file=/init/krankenhaus/sql/copy-csv.sql
+
+SET search_path TO krankenhaus;
+
+
+-- https://www.postgresql.org/docs/current/sql-copy.html
+COPY krankenhaus FROM '/init/krankenhaus/csv/krankenhaus.csv' (FORMAT csv, DELIMITER ';', HEADER true);
+COPY adresse FROM '/init/krankenhaus/csv/adresse.csv' (FORMAT csv, DELIMITER ';', HEADER true);
+COPY fachbereiche FROM '/init/krankenhaus/csv/fachbereiche.csv' (FORMAT csv, DELIMITER ';', HEADER true);
